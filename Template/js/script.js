686963b3820baf89aad6d6018ab5257e582695ba
Vladimir, Vilen Hakobyan <52594864+vilenhakobyan4444@users.noreply.github.com>
2019-08-13 07:02:58 -0700

    Update script.js

--- Template/js/script.js
+++ Template/js/script.js
@@ -1,1 +1,24 @@
+                                // DROPMENU
+         
+var click = 0;
+function menu() {
+   var dropBlock = document.getElementById("globalNav");
+   click++;
+   if(click%2!=0){
+        dropBlock.style.display = "block";
+   }
+   else{
+        dropBlock.style.display = "none";  
+   }
+}
+
+                                // NAVBAR SCROLL
+
+function func(){
+    var y = scrollY;
+     var nav = document.getElementById("navbar");
+     document.getElementById("haupt").style.top = "0"
+     nav.style.top = "0";
+     nav.style.transition = "1" + "s";
+}
 
